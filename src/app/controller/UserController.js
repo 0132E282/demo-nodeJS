15781f0edf6df1e5cea89fc16c376b6c2ce7101a
NGUYEN DEV <93367209+0132E282@users.noreply.github.com>
2024-10-14 22:56:30 +0700

    Update UserController.js

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -5,18 +5,24 @@ const bcrypt = require('bcrypt');
 class UserController {
 
     //  show the users
+    //  ?[_limit] number item paginate
+    //  ?[_page] number current page
+    //  ?[search] find all user matched
    async index(req, res){
         try {
             const options = {}
             const page = Number(req.query._page) || 1;
             const limit = Number( req.query._limit) || 10;
             const skip = (page - 1) * limit;
+            //  người dùng tìm kiếm
             if(req.query.search){
                 options['$text'] =  { $search: req.query.search }
             }
             await User.find(options).skip(skip).limit(limit)
             .then(async (users)=>{
+                // lấy tổng số lượng người dùng
                 const totalPage = await User.countDocuments() ;
+                // hiện thị thông tin trang
                 const paginate = {
                     totalPage: Math.ceil(totalPage / limit) ?? 0,
                     currentPage: page,
@@ -25,6 +31,7 @@ class UserController {
                     prevPage: (page > 1) ? page - 1 : null,
                     nextPage: ((page * limit) < totalPage) ? page + 1 : null,
                 }
+                // chuyển đổi người dùng thành object
                 users = users.map(user => user.toObject())
                 res.render('index', { users, paginate, router: `/users/${users._id}`});
             });
@@ -32,23 +39,29 @@ class UserController {
             res.render('index');
         }
     }
+    // show form create/update users
+    //  ?[id] user
    async showForm(req, res){
         try {
+            // áp dụng cho người dùng cập nhập
             if(req.params.id){
                await User.findById(req.params.id)
                .then((user) => {
+                 // gửi đi người dùng tìm kiếm, thông tinh error form khi người dùng thực không thành công middleware  cập nhập
                  res.render('update',{
                      user: user.toObject(),
                      messages: req.flash('error')
                  })
                });
             }else{
+                  //thông tinh error form khi người dùng thực không thành công middleware  tạo
                 res.render('create',{ messages: {...req.flash('error')[0]}})
             }
         } catch (error) {
-            
+                 // sữ lý lỗi
         }
     }
+    // create new user
    async create(req, res){
       try {
         const formData = {...req.body};
@@ -59,6 +72,7 @@ class UserController {
          res.redirect('alert/error');
       }
     }
+    //  message user when update or create
     notification(req, res, next){
         res.render('alertsStatus', {
             message: req.params.status == "success" ? 'thành công' : 'có lỗi xây ra',
@@ -66,6 +80,8 @@ class UserController {
             status: req.params.status
         })
     }
+    // update update 
+    // [id] users
    async edit (req, res) {
       try {
         const user = await User.updateOne({_id: req.params.id},  { $set: req.body })
@@ -74,6 +90,7 @@ class UserController {
         res.redirect('alert/error');
       }
     }
+    //  delete one user
     async remove (req, res) {
         try {
            await User.deleteOne({_id: req.params.id })
