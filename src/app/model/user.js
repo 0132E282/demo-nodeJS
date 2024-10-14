07489c980c4303f100732648d47d098a00f8751e
NGUYEN DEV <93367209+0132E282@users.noreply.github.com>
2024-10-14 22:57:52 +0700

    Update user.js

--- src/app/model/user.js
+++ src/app/model/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
+// định nghĩ user
 const Users = new Schema({
     name: { type: String, required:true , index: true },
     password: { type: String, min: 5, max: 40, required: true},
@@ -10,5 +11,6 @@ const Users = new Schema({
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
 });
+// cho phép tìm kiếm trên 2 trường name, và email
 Users.index({ name: 'text', email: 'text' });
 module.exports = mongoose.model('users', Users);
